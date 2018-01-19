70c2cbd7db56cd0ac316ec0275f9437e40ecefd9
raga <ragacan@ymail.com>
2018-01-19 17:57:31 -0500

    Adding facade pattern.

---
+++ SysArqAndDesPat/SingleResp/src/facade/MergeSort.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+/**
+ *
+ * @author raga
+ */
+public class MergeSort implements Algoritmo {
+
+    @Override
+    public void sort() {
+        System.out.println("MergeSort ....");
+    }
+    
+}
