832d5b727f989d12180ee3b0091f90048d0eabdc
Ramses Garcia <rgarcia@auvik.com>
2019-07-05 09:42:09 -0400

    Adding more functions.

--- AkkaStreams/src/main/scala/AkkaStreams.scala
+++ AkkaStreams/src/main/scala/AkkaStreams.scala
@@ -10,8 +10,16 @@ object AkkaStreams extends App {
   implicit val system = ActorSystem("AkkaStreams")
 
   //The Materializer is a factory for stream execution engines, it is the thing that makes streams run
+  //Materialization is the process of allocating all resources needed to run the computation described
+  //by a Graph (in Akka Streams this will often involve starting up Actors)
   implicit val materializer = ActorMaterializer()
 
+  // Flows are simply a description of the processing pipeline
+  // Every stream processing stage can produce a materialized value, and it is the responsibility of the user to combine them to a new type.
+  // In the above example we used toMat to indicate that we want to transform the materialized value of the source and sink,
+  // and we used the convenience function Keep.right to say that we are only interested in the materialized value of the sink.
+  // a stream can expose multiple materialized values, but it is quite common to be interested in only the value of the Source or the Sink in the stream.
+  // For this reason there is a convenience method called runWith() available for Sink, Source or Flow requiring,
 
   // ExecutionContexts, responsible for executing computations in a new thread,
   // in a pooled thread or in the current thread
@@ -38,6 +46,8 @@ object AkkaStreams extends App {
       println(exception.getMessage)
   }
 
+  val runnable1: Future[Int] = source.runWith(sink)
+
   val res = delay(5)
   res.onComplete {
     case Success(_) =>
@@ -50,3 +60,4 @@ object AkkaStreams extends App {
 
   //system.terminate()
 }
+
