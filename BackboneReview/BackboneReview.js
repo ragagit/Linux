c0db7cc545d7d10cc376146534a0c210bf95d973
ragagit <ragacan@ymail.com>
2019-08-24 17:12:54 -0400

    Adding a few more comments.

---
+++ BackboneReview/BackboneReview.js
@@ -0,0 +1,51 @@
+/*
+Backbone.js organizes your code
+Creates clean and efficient code
+Based on MVC pattern
+Models - Data
+Views - Display
+Controllers - Transfer data model to the view. Backbone doesn't have Controllers
+Backbone has siz components:
+    Models
+    Views
+    Collections, Javascript array
+    Events
+    Routers - Navigations
+Javascript objects
+Simple key/value pairs of JS methods & properties
+You can create your own methids & properties
+No mixed up code
+No mixing JS into your HTML
+Code is easier to mainatin & reuse
+Backbone.js is a libtrary not a framework
+Framework needs configuration.
+Libraries have pre-built functions ready
+Highly customizable
+Most lightweight in Javascript MVC
+
+- Setting up files
+Modules
+    A way of referring to single .js files
+    Modules contain a particular piece of component code
+    Modules make large codebase easier to read
+    Modules are GREAT for team situations
+
+- Watching for changes
+Initialize() - Watches for changes
+get() - find a specofoc model property
+set() - change or add new properties to the model
+on() - trigger the changes
+
+- Views 
+Contain the rukes for moving data into html
+Views create templates thanks to Underscore
+Youc an use other templates in Backbone
+Use render() to build the HTML structure.
+render() also sends model data to the web page
+Model instance data gets stored in an article tag
+Collection data gets stored in a section tag
+
+- Routes
+Key/Value pairs
+
+*/
